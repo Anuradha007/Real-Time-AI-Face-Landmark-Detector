230f1dff9f57acf3b13503c74930dff8df6823ec
Anuradha Raj <shikhhaayadav@gmail.com>
2025-10-22 06:09:05 +0530

    Setup Triangle path and point drawing

--- src/utilities.js
+++ src/utilities.js
@@ -2643,7 +2643,21 @@ export const TRIANGULATION = [
   255,
 ];
 
+// Triangle drawing method
+const drawPath = (ctx, points, closePath) => {
+  const region = new Path2D();
+  region.moveTo(points[0][0], points[0][1]);
+  for (let i = 1; i < points.length; i++) {
+    const point = points[i];
+    region.lineTo(point[0], point[1]);
+  }
 
+  if (closePath) {
+    region.closePath();
+  }
+  ctx.strokeStyle = "grey";
+  ctx.stroke(region);
+};
 
 // Drawing Mesh
 export const drawMesh = (predictions, ctx) => {
